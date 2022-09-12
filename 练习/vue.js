e2b5d068b11d223bd7294691f630f2b2bfc3efce
xzh <1322739583@qq.com>
2022-09-12 11:17:41 +0800

    数据绑定 图书练习

---
+++ 练习/vue.js
@@ -0,0 +1,40 @@
+var app = new Vue({
+    el: "#box",
+    data: {
+        message: "hello",
+        citys: ["北京", "上海", "广州", "深圳"],
+        letters: ["A", "B", "C", "D", "E"],
+        numbers: [1, 2, 3, 4, 5],
+        books: [
+            {id: "1", time: "2019-10", name: "Java", num: 1, price: 50.00},
+            {id: "2", time: "2018-5", name: "JavaScript", num: 1, price: 33.00},
+            {id: "3", time: "2020-6", name: "CSS", num: 1, price: 20.00},
+            {id: "4", time: "2020-10", name: "Html", num: 1, price: 10.00}
+        ]
+    },
+    methods: {
+        increment(index){
+             return this.books[index].num++
+        },
+        decrement(index){
+             let temp=this.books[index].num
+             if (temp>0) {
+                 return this.books[index].num--
+             }
+        },
+        removeBookItem(index){
+              this.books.splice(index,1)
+        }
+    },
+    computed:{
+      totalPrice(){
+         return this.books.map(book => book.num*book.price).reduce((prev,cur)=>prev+cur)
+      }
+    },
+    //有个s别写错了
+    filters: {
+        formatPrice(price) {
+            return "￥" + price.toFixed(2)
+        }
+    }
+})
