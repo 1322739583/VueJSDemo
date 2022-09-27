2c4895902aafc577d058f4057dbd8670fd95d12d
xzh <1322739583@qq.com>
2022-09-27 15:13:50 +0800

    supermall项目 添加vue.config.js和.editerconfig

---
+++ supermall/vue.config.js
@@ -0,0 +1,14 @@
+module.exports={
+    configureWebpack:{
+        resolve:{
+            //配置别名，@表示src
+            alias:{
+                "assets":"@assets",
+                "common":"@common",
+                "components":"@components",
+                "network":"@network",
+                "views":"@views"
+            }
+        }
+    }
+}
