14664598c19f14a6f818709a1b67351d589fcc4d
xzh <1322739583@qq.com>
2022-09-28 17:48:24 +0800

    supermall 完成navbar的引入

---
+++ supermall/src/components/common/navbar/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="navBar">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+.navBar{
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+  box-shadow: 0 1px 1px rgba(100,100,100,0.1);
+  /*background-color: var(--color-tint);*/
+}
+
+.left ,.right{
+  width: 60px;
+  height: 44px;
+  /*background-color: lightgreen;*/
+}
+
+.center{
+  flex: 1;
+  /*flex-grow: 1;*/
+  /*background-color: lightblue;*/
+}
+
+
+</style>
--- supermall/src/views/home/Home.vue
+++ supermall/src/views/home/Home.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>home</div>
+<!--  <router-view>-->
+<!--  </router-view>-->
+  <div class="home">
+    <navbar class="nav-home">
+<!--      <div slot="right">123</div>-->
+      <div slot="center" >购物街</div>
+<!--      <div slot="left">123</div>-->
+    </navbar>
+  </div>
+
+<!--  <div>home</div>-->
 </template>
 
 <script>
+import Navbar from "@/components/common/navbar/Navbar";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {Navbar}
 }
 </script>
 
 <style scoped>
+.nav-home{
+  background-color: var(--color-tint);
+  color: white;
+}
 
 </style>
