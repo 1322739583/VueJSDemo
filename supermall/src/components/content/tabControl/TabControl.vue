f83d8ff8a69caabdf420363fbdaeed12a882bd28
xzh <1322739583@qq.com>
2022-09-30 16:34:56 +0800

    supermall 实现tabControl组件

---
+++ supermall/src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,59 @@
+<template>
+   <div class="tab-control">
+     <div v-for="(item,index) in titles" class="tab-control-item" :class="{active:index===curIndex}" @click="itemClick(index)">
+       <span>{{item}}</span>
+     </div>
+   </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props:{
+    titles:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+  data(){
+    return{
+      curIndex:0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.curIndex=index
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-control{
+    display: flex;
+    text-align: center;
+    background-color: white;
+    position: sticky;
+    top: 44px;
+
+  }
+  .tab-control-item{
+    flex: 1;
+    height: 40px;
+    line-height: 40px;
+  }
+  .tab-control-item span{
+    padding: 3px 3px 5px;
+  }
+
+  .active{
+    color: var(--color-tint);
+
+  }
+
+  .active span{
+    border-bottom: 3px solid var(--color-tint);
+  }
+</style>
