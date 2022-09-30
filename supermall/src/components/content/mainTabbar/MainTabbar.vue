d2b883c80e4b9af50c62e77350b0ff701046a0be
xzh <1322739583@qq.com>
2022-09-30 15:01:29 +0800

    supermall 图片目录修改

--- supermall/src/components/content/mainTabbar/MainTabbar.vue
+++ supermall/src/components/content/mainTabbar/MainTabbar.vue
@@ -1,29 +1,29 @@
 <template>
   <tabbar>
     <tabbar-item path="/home"   >
-      <img slot="item-icon" src="@/assets/img/home_normal.svg" alt="">
-      <img slot="item-icon-active" src="@/assets/img/home_selected.svg" alt="">
+      <img slot="item-icon" src="@/assets/img/tabbar/home_normal.svg" alt="">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/home_selected.svg" alt="">
       <div slot="item-text">
         首页
       </div>
     </tabbar-item>
     <tabbar-item path="/category"   >
-      <img slot="item-icon" src="@/assets/img/category_normal.svg" alt="">
-      <img slot="item-icon-active" src="@/assets/img/category_selected.svg" alt="">
+      <img slot="item-icon" src="@/assets/img/tabbar/category_normal.svg" alt="">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/category_selected.svg" alt="">
       <div slot="item-text">
         分类
       </div>
     </tabbar-item>
     <tabbar-item path="/shopcar"  >
-      <img slot="item-icon" src="@/assets/img/shopcar_normal.svg" alt="">
-      <img slot="item-icon-active" src="@/assets/img/shopcar_selected.svg" alt="">
+      <img slot="item-icon" src="@/assets/img/tabbar/shopcar_normal.svg" alt="">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/shopcar_selected.svg" alt="">
       <div slot="item-text">
         购物车
       </div>
     </tabbar-item>
     <tabbar-item path="/profile"  >
-      <img slot="item-icon" src="@/assets/img/user_normal.svg" alt="">
-      <img slot="item-icon-active" src="@/assets/img/user_selected.svg" alt="">
+      <img slot="item-icon" src="@/assets/img/tabbar/user_normal.svg" alt="">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/user_selected.svg" alt="">
       <div slot="item-text">
         我的
       </div>
