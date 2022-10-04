edc2fd6c381affecda441b24446d6e824dae65c4
xzh <1322739583@qq.com>
2022-10-04 17:02:22 +0800

    supermall better-scroll的安装和基本使用

--- supermall/src/components/content/goods/GoodsList.vue
+++ supermall/src/components/content/goods/GoodsList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goodsList" >
-        <goods-list-item v-for="item in goods" :goods-item="item"/>
+        <goods-list-item v-for="item in goods" :goods-item="item" key="item"/>
   </div>
 </template>
 
