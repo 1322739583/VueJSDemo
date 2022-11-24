c472f45957e4e718bd20c2b7909f543118347a7d
xzh <1322739583@qq.com>
2022-11-24 16:48:59 +0800

    完成tabbar的手动实现

--- hy-trip/src/components/tabbar/Tabbar.vue
+++ hy-trip/src/components/tabbar/Tabbar.vue
@@ -1,29 +1,38 @@
-<script setup >
-
-</script>
+<script setup></script>
 
 <template>
-   <div class="tabbar">
-        <div class="tabbar-item">
-          <span>首页</span>
-          <img src="@/assets/img/tabbar/tab_home.png" alt=""/>
-        </div>      
-        <div class="tabbar-item">
-          <span>收藏</span>
-          <img src="@/assets/img/tabbar/tab_favor.png" alt=""/>
-        </div>
-        <div class="tabbar-item">
-          <span>订单</span>
-          <img src="@/assets/img/tabbar/tab_order.png" alt=""/>
-        </div>
-        <div class="tabbar-item">
-          <span>消息</span>
-          <img src="@/assets/img/tabbar/tab_message.png" alt=""/>
-
-        </div>
-   </div>
+  <div class="tabbar">
+    <div class="tabbar-item">
+      <span>首页</span>
+      <img src="@/assets/img/tabbar/tab_home.png" alt="" />
+    </div>
+    <div class="tabbar-item">
+      <span>收藏</span>
+      <img src="@/assets/img/tabbar/tab_favor.png" alt="" />
+    </div>
+    <div class="tabbar-item">
+      <span>订单</span>
+      <img src="@/assets/img/tabbar/tab_order.png" alt="" />
+    </div>
+    <div class="tabbar-item">
+      <span>消息</span>
+      <img src="@/assets/img/tabbar/tab_message.png" alt="" />
+    </div>
+  </div>
 </template>
 
-<style  scoped>
-    
+<style lang="less" scoped>
+.tabbar{
+  display: flex;
+  justify-content:space-around;
+  height: 48px;
+  width: 100%;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+ 
+}
+
+
 </style>
